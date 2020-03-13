831a8afeb7aabbb50ebf0686734b4dc552c48973
Orin Prindle <47831332+voshyk@users.noreply.github.com>
2020-03-13 13:21:51 -0600

    Implemented DashAttack and Multiplier/Percentage Attack
    On a gamepad press the West ("x") button and you should dash forward, spawning a blue hitbox. When you press the North ("y") button you should spawn a red hitbox. These all have cool downs as well. I have placed a dummy in the game for you test on. ">MULTIPLIER HIT" should print on the debug console when the dummy is hit with the melee attack. ">DASH ATTACK HIT" should print to the debug console when the dummy is hit with a dash attack.

--- Toybox Rumble/Assets/Scripts/PlayerController.cs
+++ Toybox Rumble/Assets/Scripts/PlayerController.cs
@@ -5,37 +5,98 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    //adding useless comment to test push
+    //private variables
     private Vector2 inputMovement;
     private Vector2 inputRotation;
     private bool rightStickActive;
     private Rigidbody rb;
+    private int countFramesDA;
+    private int countFramesDash;
+    private int countFramesMultiplier;
+    private bool hasDashed;
 
+    //public variables
     public float moveSpeed = 10f;
     public float rotateSpeed = 10f;
     public float dashSpeed = 10f;
+    public float dashAttackSpeed = 10f;
+    public float multiplier = 0f;
+    public GameObject dashAttackCollider;
+    public GameObject multiplierCollider;
     
-
-
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         rightStickActive = false;
+        dashAttackCollider.SetActive(false);
+        multiplierCollider.SetActive(false);
+        countFramesDA = 0;
+        countFramesDash = 0;
+        countFramesMultiplier = 0;
+        hasDashed = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //allows the user to dash 35 frames after originally dashing
+        if (hasDashed == true)
+        {
+            countFramesDash++;
+            if (countFramesDash == 35)
+            {
+                hasDashed = false;
+                countFramesDash = 0;
+                Debug.Log(">DASH READY\t" + hasDashed);
+            }
+        }
+
+        //turns the dashAttackCollider off after 100 frames
+        if (dashAttackCollider.activeSelf == true)
+        {
+            countFramesDA++;
+            if (countFramesDA == 100)
+            {
+                dashAttackCollider.SetActive(false);
+                countFramesDA = 0;
+                Debug.Log(">DA READY\t" + dashAttackCollider.activeSelf);
+            }
+        }
+
+        //turns the multiplierCollider off after 15 frames
+        if (multiplierCollider.activeSelf == true)
+        {
+            countFramesMultiplier++;
+            if (countFramesMultiplier == 15)
+            {
+                multiplierCollider.SetActive(false);
+                countFramesMultiplier = 0;
+                Debug.Log(">Melee READY\t" + multiplierCollider.activeSelf);
+            }
+        }
+
+        //determines where to look and why
         if (Input.GetAxis("HorizontalRight") >= -.19 && Input.GetAxis("HorizontalRight") <= .19
             && Input.GetAxis("VerticalRight") >= -.19 && Input.GetAxis("VerticalRight") <= .19)
         {
             rightStickActive = false;
         }
-            
 
         Move();
         Look();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "DashAttackHitBox")
+        {
+            Debug.Log(">DASH ATTACK HIT");
+        }
+        if(other.tag == "MultiplierHitBox")
+        {
+            Debug.Log(">MULTIPLIER HIT");
+        }
     }
 
     //Moves the player in the direction of the left stick
@@ -83,7 +144,29 @@ public class PlayerController : MonoBehaviour
      */
     private void OnDash(InputValue v)
     {
-        Vector3 dash = new Vector3(inputMovement.x, 0, inputMovement.y);
-        rb.AddForce(dash * dashSpeed);
+        if (hasDashed == false)
+        {
+            Vector3 dash = new Vector3(inputMovement.x, 0, inputMovement.y);
+            rb.AddForce(dash * dashSpeed);
+            hasDashed = true;
+        }
+    }
+
+    private void OnDashAttack(InputValue v)
+    {
+        if(dashAttackCollider.activeSelf == false)
+        {
+            Vector3 dash = new Vector3(inputMovement.x, 0, inputMovement.y);
+            rb.AddForce(dash * dashAttackSpeed);
+            dashAttackCollider.SetActive(true);
+        }
+    }
+
+    private void OnMultiplierAttack(InputValue v)
+    {
+        if(multiplierCollider.activeSelf == false)
+        {
+            multiplierCollider.SetActive(true);
+        }
     }
 }
